1e6f9a14e03d4757b66bbe2f08d40340b1837e1f
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-10-29 20:59:31 -0600

    dadas

--- MetodosHibridos.cpp
+++ MetodosHibridos.cpp
@@ -8,8 +8,10 @@ void Humano::agregarAmigos(Humano arrayDeLaVida[], int cantHumanosActual){
             break;
         }else if (candidatoAmigo->ID!=ID && candidatoAmigo->pais==pais && 
         (candidatoAmigo->creencia==creencia || candidatoAmigo->apellido==apellido || candidatoAmigo->profesion==profesion)){
-            amigos->insertarFinal(candidatoAmigo);
-            cantidadAmigos--;
+            if(!amigos->tieneAmigo(candidatoAmigo->ID)){
+                amigos->insertarFinal(candidatoAmigo);
+                cantidadAmigos--;
+            }
         }
     }
 }
@@ -19,14 +21,30 @@ void generarAmigosYPecados(Humano arrayDeLaVida[], int cantHumanosActual){
     for (int i=0; i < cantHumanosActual; i++){
         humano=&arrayDeLaVida[i];
         humano->agregarAmigos(arrayDeLaVida, cantHumanosActual);
-        
+        humano->inicializarPecados();
     }
 }
 
-void Humano::inicializarPecados(){
-
+int crearGeneracionHumanos(int cantidad, Humano arrayDeLaVida[], int cantHumanosActual){
+    if (cantHumanosActual+cantidad>1000){
+        return 1001;
+    }
+    int ID;
+    string nombre, apellido, profesion, creencia, pais, nacimiento;
+    for (int i = cantHumanosActual+1; i < cantidad+cantHumanosActual; i++){
+        nombre=extraerLineaAleatoria("nombres.txt",1000);
+        apellido=extraerLineaAleatoria("apellido.txt",30);
+        profesion=extraerLineaAleatoria("profesiones.txt",30);
+        creencia=extraerLineaAleatoria("creencias.txt",30);
+        pais=extraerLineaAleatoria("paises.txt",30);
+        //nacimiento=generarFechaYHoraActual();
+        do{
+            ID=generarNumerosAleatorios(99999);
+        } while (validarID(ID));
+        
+        
+        arrayDeLaVida[i]=* new Humano(ID,nombre,apellido,pais,creencia,profesion,nacimiento,generarNumerosAleatorios(100));
+    }
+    return cantHumanosActual;
 }
 
-void Humano::agregarPecado(){
-
-}
