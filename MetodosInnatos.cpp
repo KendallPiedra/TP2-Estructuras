02b4583f8325344f307c5dc61cb633aaba9466eb
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-09 22:37:17 -0600

    werewr

--- MetodosInnatos.cpp
+++ MetodosInnatos.cpp
@@ -374,9 +374,9 @@ void ListaBesties::imprimirConPecados(){
 // BITACORA DE CONDENACIÓN ---------------------------------------------------------------------------------------------
 void BitacoraCondenacion::insertarFinal(int indice, Humano * humano, string pecado){
     if (primerNodo==NULL)
-	    primerNodo=ultimoNodo=new NodoBitacora(indice, humano, pecado, obtenerFechaYHoraActual());
+	    primerNodo=ultimoNodo=new NodoBitacora(indice, humano, pecado, obtenerFechaYHoraActual(), extraerDemonioConPecado(pecado));
     else{
-	    ultimoNodo->siguiente= new NodoBitacora(indice, humano, pecado, obtenerFechaYHoraActual());
+	    ultimoNodo->siguiente= new NodoBitacora(indice, humano, pecado, obtenerFechaYHoraActual(), extraerDemonioConPecado(pecado));
 	    ultimoNodo->siguiente->anterior=ultimoNodo;
 	    ultimoNodo=ultimoNodo->siguiente; 
     }
