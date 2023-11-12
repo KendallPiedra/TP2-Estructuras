601958da9652a217b7f2485a417a76ab49bdc970
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-12 16:26:39 -0600

    adsadasd

--- MetodosHibridos.cpp
+++ MetodosHibridos.cpp
@@ -433,8 +433,8 @@ void Infierno::enviarDemonio(int posicionD){
     // cout<<"enviardemonio"<<endl;
     // cout<<demonios[posicionD]->nombre<<endl;
     string pecado= demonios[posicionD]->pecado;
-    // cout<<pecado<<endl;
-    Humano** condenados =ADLV->sacarPorcentajeMasPecador(pecado);
+    cout<<pecado<<endl;
+    Humano** condenados = ADLV->sacarPorcentajeMasPecador(pecado);
     // cout<<"Se saca el porcentaje mas pecador"<<endl;
     int cantidadCondenados = PorcentajeACantidad(5, ADLV->cantidadHumanos);
     // cout<<"cantidad condenados: "<< cantidadCondenados<<endl;
