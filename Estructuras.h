0530f06ab505c85e7d1df1e25fe0262034a3ac53
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-12 15:26:48 -0600

    asdsa

--- Estructuras.h
+++ Estructuras.h
@@ -168,7 +168,7 @@ struct ArbolDeLaVida{
     Humano** sacarPorcentajeMasPecador(string pecado);
     int * sacarIndicesOrdenadosSegunPecado(string pecado);
 
-    Familia * sacarFamiliacompleta(string apellido, string pais);
+    Familia * sacarFamiliaCompleta(string apellido, string pais);
 
 };
 
