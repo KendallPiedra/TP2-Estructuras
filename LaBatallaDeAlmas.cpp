308fa4b24b28a00ff7baa85d4e4997ed28d3b89e
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-12 10:14:27 -0600

    Update LaBatallaDeAlmas.cpp

--- LaBatallaDeAlmas.cpp
+++ LaBatallaDeAlmas.cpp
@@ -166,7 +166,46 @@ int main(int argc, char const *argv[])
     ArbolDeLaVida * arbolDeLaVida= new ArbolDeLaVida();
     arbolDeLaVida->crearGeneracionHumanos(stoi(cantidadInicial));
     arbolDeLaVida->imprimir();
-
+    int opcion=1;
+    do{
+        // "1: Crear Generación de Humanos"
+	    // "2: Publicar en Redes Sociales"
+	    // "3: La Condenación"
+	    // "4: El Infierno"
+	    // "5: La Salvación"
+	    // "6: El Cielo"
+	    // "7: Consultas"
+	    // "0: SALIR"
+        opcion=menuPrincipal();
+        switch (opcion){
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 0:
+            break;
+        default:
+            
+            break;
+        }
+    } while (opcion!=0);
 
     return 0;
 }
